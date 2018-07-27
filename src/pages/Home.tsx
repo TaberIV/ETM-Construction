b2c89755112f82a8e9449c6eec67cfdd1d398195
Taber McFarlin <tabermcfarlin@gmail.com>
2018-07-27 17:59:49 -0400

    Deleted duplicate picture

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,22 +24,76 @@ export default () => (
         organized which make for pleasant and productive working conditions.
       </p>
       <p>
-        ETM Construction is an honest, reliable and quality firm. When we bid a
-        project there are no hidden costs, the scope of work is broken down so
-        clients know exactly where every dollar is applied. While most projects
-        have detailed architectural plans allowing a firm cost to be obtained,
-        some do not which opens the door to shocking extras; at ETM Construction
-        we attach REAL allowances to the unknown because we are straightforward
-        and our reputation depends on it.
+        Located on Connecticut's Gold Coast, ETM Construction is a family owned
+        and operated business that has been serving local home owners for over
+        twenty five years.
       </p>
       <p>
-        ETM Construction is an honest, reliable and quality firm. When we bid a
-        project there are no hidden costs, the scope of work is broken down so
-        clients know exactly where every dollar is applied. While most projects
-        have detailed architectural plans allowing a firm cost to be obtained,
-        some do not which opens the door to shocking extras; at ETM Construction
-        we attach REAL allowances to the unknown because we are straightforward
-        and our reputation depends on it.
+        Whether it's new construction or a renovation, ETM Construction takes
+        pride on delivering high quality workmanship, within a realistic and
+        agreed upon timeframe and budget. My brother Alan and I have been in the
+        building business for over twenty–five years. We are both accomplished
+        carpenters. We have been schooled in the entire building process by
+        proficient local craftsman. This knowledge makes us exceptional project
+        managers who supply a high quality project on time and on budget from
+        foundation to punch list. ETM construction only hires local
+        subcontractors who carry the proper insurances, licenses and permits.
+        Our jobsites are well supervised we keep them safe, smoke free and
+        organized which make for pleasant and productive working conditions.
+      </p>
+      <p>
+        Located on Connecticut's Gold Coast, ETM Construction is a family owned
+        and operated business that has been serving local home owners for over
+        twenty five years.
+      </p>
+      <p>
+        Whether it's new construction or a renovation, ETM Construction takes
+        pride on delivering high quality workmanship, within a realistic and
+        agreed upon timeframe and budget. My brother Alan and I have been in the
+        building business for over twenty–five years. We are both accomplished
+        carpenters. We have been schooled in the entire building process by
+        proficient local craftsman. This knowledge makes us exceptional project
+        managers who supply a high quality project on time and on budget from
+        foundation to punch list. ETM construction only hires local
+        subcontractors who carry the proper insurances, licenses and permits.
+        Our jobsites are well supervised we keep them safe, smoke free and
+        organized which make for pleasant and productive working conditions.
+      </p>
+      <p>
+        Located on Connecticut's Gold Coast, ETM Construction is a family owned
+        and operated business that has been serving local home owners for over
+        twenty five years.
+      </p>
+      <p>
+        Whether it's new construction or a renovation, ETM Construction takes
+        pride on delivering high quality workmanship, within a realistic and
+        agreed upon timeframe and budget. My brother Alan and I have been in the
+        building business for over twenty–five years. We are both accomplished
+        carpenters. We have been schooled in the entire building process by
+        proficient local craftsman. This knowledge makes us exceptional project
+        managers who supply a high quality project on time and on budget from
+        foundation to punch list. ETM construction only hires local
+        subcontractors who carry the proper insurances, licenses and permits.
+        Our jobsites are well supervised we keep them safe, smoke free and
+        organized which make for pleasant and productive working conditions.
+      </p>
+      <p>
+        Located on Connecticut's Gold Coast, ETM Construction is a family owned
+        and operated business that has been serving local home owners for over
+        twenty five years.
+      </p>
+      <p>
+        Whether it's new construction or a renovation, ETM Construction takes
+        pride on delivering high quality workmanship, within a realistic and
+        agreed upon timeframe and budget. My brother Alan and I have been in the
+        building business for over twenty–five years. We are both accomplished
+        carpenters. We have been schooled in the entire building process by
+        proficient local craftsman. This knowledge makes us exceptional project
+        managers who supply a high quality project on time and on budget from
+        foundation to punch list. ETM construction only hires local
+        subcontractors who carry the proper insurances, licenses and permits.
+        Our jobsites are well supervised we keep them safe, smoke free and
+        organized which make for pleasant and productive working conditions.
       </p>
     </main>
   </React.Fragment>
