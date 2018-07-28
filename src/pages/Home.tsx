88bc984d4ba36aa79939c9604988492295cd46c3
Taber McFarlin <tabermcfarlin@gmail.com>
2018-07-28 00:46:24 -0400

    Adds SCSS

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,19 +10,6 @@ export default () => (
         and operated business that has been serving local home owners for over
         twenty five years.
       </p>
-      <p>
-        Whether it's new construction or a renovation, ETM Construction takes
-        pride on delivering high quality workmanship, within a realistic and
-        agreed upon timeframe and budget. My brother Alan and I have been in the
-        building business for over twenty–five years. We are both accomplished
-        carpenters. We have been schooled in the entire building process by
-        proficient local craftsman. This knowledge makes us exceptional project
-        managers who supply a high quality project on time and on budget from
-        foundation to punch list. ETM construction only hires local
-        subcontractors who carry the proper insurances, licenses and permits.
-        Our jobsites are well supervised we keep them safe, smoke free and
-        organized which make for pleasant and productive working conditions.
-      </p>
     </main>
   </React.Fragment>
 );
