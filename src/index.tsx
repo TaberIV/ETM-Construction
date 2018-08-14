e1dbe24f28117fbaa7e3225e005014c21acd7fd9
Taber McFarlin <tabermcfarlin@gmail.com>
2018-08-14 16:38:34 -0400

    Moved to ts folder

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 import "./styles/css/main.css";
+import App from "./ts/App";
 
 ReactDOM.render(
   <BrowserRouter>
