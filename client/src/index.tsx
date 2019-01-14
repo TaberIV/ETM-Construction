53f1ca104b39768896a23d192bb8d1204d411843
Taber McFarlin <tabermcfarlin@gmail.com>
2019-01-14 17:28:19 -0500

    HashRouter

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import "./styles/css/main.css";
 import App from "./ts/App";
 
